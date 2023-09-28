f590825ceaebf15ea4637dc8c5c3e52f53772a0d
Sumayya07 <sumayyasiddiqui7173@gmail.com>
2023-09-28 11:06:07 +0530

    Changes

---
+++ Heka Healthy You/Controller/MedicalAstroglogyViewControllers/PremiumCareViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PremiumCareViewController.swift
+//  Heka Healthy You
+//
+//  Created by Sumayya Siddiqui on 28/09/23.
+//
+
+import UIKit
+
+class PremiumCareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
